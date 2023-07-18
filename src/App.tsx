93fe84a3899bb232c1dda05db863d1222ba289e4
Slavniukp <p.slavniuk@spheremall.com>
2023-07-18 19:18:58 +0300

    initialized RTK toolkit example

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { userSlice } from './store/reducers/UserSlice';
+import { fetchUsers } from './store/reducers/ActionCreators';
+
+function App() {
+  const dispatch = useAppDispatch()
+  const {users, isLoading, error} = useAppSelector(state => state.userReducer);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [])
+
+  return (
+    <div className="App">
+      {isLoading && <h1>It's loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {JSON.stringify(users, null, 2)}
+    </div>
+  );
+}
+
+export default App;
